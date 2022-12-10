28bc0d9b62858ddd112cba925d7c222a3a4a9676
Doson <karbushev-1991@mail.ru>
2022-12-10 16:14:11 +0700

    checkpoint

--- monitoring.py
+++ monitoring.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from Ichimoku import spanA, spanB, tenkan_Sen, kijun_Sen, chikou_Span
 import pandas as pd
 import inspect
@@ -7,46 +8,57 @@ import pandas_ta as ta
 
 
 class Monitor:
-
-    def __init__(self, data: pd.DataFrame, ):
-        self.position = 0
-        self.data = data
-
-    def signal_buy(self):
-        if self.data.Close[-1] > self.span_A[-1] > self.span_B[-1] and \
-                self.tenkan_sen[-1] > self.kijun_sen[-1] \
-                and self.chikou_span[-1] > self.data.High[-26]:
-            self.buy()
-
-    def signal_sell(self):
-        if self.position:
-            if (self.data.Low[-1] < self.kijun_sen[-1] and self.tenkan_sen[-1] < self.kijun_sen[-1]) \
-                    or self.data.Low[-1] < self.span_B[-1] \
-                    or (self.data.Low[-1] < self.kijun_sen[-1] and self.tenkan_sen[-1] > self.kijun_sen[-1] and
-                        self.data.Close[-2] > self.tenkan_sen[-1]):  # добавил - уменьшилась просадка
-                # and (self.data.Close[-1] < self.span_A[-1] or self.data.Close[-1] < self.span_B[-1]):
-                self.position.close()
-
-
-def get_ichimoku(data, tenkan_param, kijun_param, senkou_param) -> pd.DataFrame:
-    ichimoku = ta.ichimoku(data.High, data.Low, data.Close,
-                           tenkan=tenkan_param, kijun=kijun_param, senkou=senkou_param)
-    return ichimoku[0]
-
-
-def main():
     tenkan_param = 9
     kijun_param = 26
     senkou_param = 52
 
+    def __init__(self, data: pd.DataFrame):
+        self.data = data
+        ichimoku = self.get_ichimoku()
+        self.span_A = ichimoku.iloc(axis=1)[0]
+        self.span_B = ichimoku.iloc(axis=1)[1]
+        self.tenkan_sen = ichimoku.iloc(axis=1)[2]
+        self.kijun_sen = ichimoku.iloc(axis=1)[3]
+        self.chikou_span = ichimoku.iloc(axis=1)[4]
+
+    def signal_buy(self):
+        if self.data.Close[-1] > self.span_A[-1] > self.span_B[-1] and \
+                self.tenkan_sen[-1] > self.kijun_sen[-1] \
+                and self.chikou_span[-27] > self.data.High[-27]:
+            # self.buy()
+            print('BUY!!!')
+
+    def signal_sell(self):
+        if (self.data.Low[-1] < self.kijun_sen[-1] and self.tenkan_sen[-1] < self.kijun_sen[-1]) \
+                or self.data.Low[-1] < self.span_B[-1] \
+                or (self.data.Low[-1] < self.kijun_sen[-1] < self.tenkan_sen[-1] < self.data.Close[-2]):  # добавил - уменьшилась просадка
+            # and (self.data.Close[-1] < self.span_A[-1] or self.data.Close[-1] < self.span_B[-1]):
+            # self.position.close()
+            print("SELL!!!")
+
+    def get_ichimoku(self):
+        ichimoku = ta.ichimoku(self.data.High, self.data.Low, self.data.Close,
+                               tenkan=self.tenkan_param, kijun=self.kijun_param, senkou=self.senkou_param)
+        return ichimoku[0]
+
+    def view(self):
+        df = pd.concat([self.data.Close, #self.data.iloc(axis=1)[:4],
+                        self.span_A,
+                        self.span_B,
+                        self.tenkan_sen,
+                        self.kijun_sen,
+                        self.chikou_span],
+                       axis=1, join='inner')
+        plt.plot(df)
+        plt.show()
+
+
+def main():
     tiker = yf.Ticker("BABA")
     tiker_data = tiker.history(period='1y', interval='1d')
-    ichimoku = get_ichimoku(tiker_data, tenkan_param, kijun_param, senkou_param)
-    spanA = ichimoku.iloc(axis=1)[0]
-    spanB = ichimoku.iloc(axis=1)[1]
-    tenkan_Sen = ichimoku.iloc(axis=1)[2]
-    kijun_Sen = ichimoku.iloc(axis=1)[3]
-    chikou_Span = ichimoku.iloc(axis=1)[4]
+    ticker1 = Monitor(tiker_data)
+    ticker1.signal_buy()
+    # ticker1.view()
 
 
 if __name__ == '__main__':
