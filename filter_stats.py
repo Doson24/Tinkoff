90135881ca5515aaac82bd2f9cba24dfb7d1ce85
Doson <karbushev-1991@mail.ru>
2022-12-11 22:29:14 +0700

    добавил в парамаетры в интервал

--- filter_stats.py
+++ filter_stats.py
@@ -7,7 +7,7 @@ pd.set_option('display.max_columns', None)
 
 
 def save_json(dict_params:dict):
-    with open('settings.json', 'w',  encoding='utf8') as f:
+    with open('settings_track.json', 'w', encoding='utf8') as f:
         json.dump(dict_params, f, indent=6)
 
 
@@ -17,7 +17,9 @@ def transform_param(df: pd.Series) ->dict:
     '''
     param = dict()
     for i in range(len(df)):
-        param[df.iloc[i][0]] = {'position': False, 'settings': [str(df.iloc[i][1]), str(df.iloc[i][2]), str(df.iloc[i][3])]}
+        param[df.iloc[i][0]] = {'position': False,
+                                'settings': [str(df.iloc[i][1]), str(df.iloc[i][2]), str(df.iloc[i][3])],
+                                'interval': df.iloc[i][4].strip()}
     print(param)
 
     return param
@@ -27,15 +29,17 @@ def main():
     df = pd.read_csv('stats.csv')
     df.Start = pd.to_datetime(df.Start)
 
+    #фильтр на интервал
+    filter_interval = df[df['interval'] == ' CANDLE_INTERVAL_DAY']
     # Поиск макс Return [%]
-    df_max_return = df.loc[df.groupby(by=df.ticker)['Return [%]'].idxmax()]
-    res = df_max_return[['ticker', 'Start', 'ten', 'kij', 'sen', "Return [%]", 'Buy & Hold Return [%]']]
+    df_max_return = filter_interval.loc[filter_interval.groupby(by=df.ticker)['Return [%]'].idxmax()]
+    res = df_max_return[['ticker', 'Start', 'ten', 'kij', 'sen', "Return [%]", 'Buy & Hold Return [%]', 'interval']]
 
     # save file
     res.to_csv('filter_stat.csv', index=False)
 
     df_max_return.index = df_max_return.ticker
-    param = df_max_return[['ticker', 'ten', 'kij', 'sen']]
+    param = df_max_return[['ticker', 'ten', 'kij', 'sen', 'interval']]
     # print(param.to_dict('index'))
 
     dict_params = transform_param(param)
