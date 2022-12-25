3dfd5073823ef40f1ea0e0f7eb70367cc809102f
Doson <karbushev-1991@mail.ru>
2022-12-25 21:23:36 +0700

    Create

---
+++ Train_test_param.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from main import open_file_figies
+
+
+def train_test(df: pd.DataFrame):
+    ...
+    df
+
+
+
+if __name__ == '__main__':
+    tickers = open_file_figies().Name
