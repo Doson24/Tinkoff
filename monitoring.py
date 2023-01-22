2306086a87b16b862b8fec04e26eb2e3bc30c3ce
Doson <karbushev-1991@mail.ru>
2023-01-23 00:02:05 +0700

    добавил загрузку OHLC через Тинькофф, реализовал покупку/продажу через брокера

--- monitoring.py
+++ monitoring.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import os
 import time
 
 import matplotlib.pyplot as plt
@@ -9,6 +10,10 @@ import inspect
 from Ichimoku import Ichimoku_cross
 import yfinance as yf
 import pandas_ta as ta
+from main import Ticker
+from tinkoff.invest.utils import now
+from datetime import timedelta
+from tinkoff.invest import CandleInterval, Client, HistoricCandle, RequestError, OrderDirection, OrderType
 
 
 class MonitoringTiker:
@@ -16,13 +21,20 @@ class MonitoringTiker:
     # kijun_param = 26
     # senkou_param = 52
 
-    def __init__(self, tiker, position, tenkan_param, kijun_param, senkou_param):
-        self.tenkan_param = int(tenkan_param)
-        self.kijun_param = int(kijun_param)
-        self.senkou_param = int(senkou_param)
-        self.position = position
-        self.tiker = tiker
-        self.data = self.download_yf()
+    def __init__(self, tiker: str, figi: str, client_tinkoff,
+                 from_day, position, quantity,
+                 tenkan_param, kijun_param, senkou_param,
+                 until_day: datetime = now(),
+                 interval=CandleInterval.CANDLE_INTERVAL_HOUR
+                 ):
+
+        self.tenkan_param, self.kijun_param, self.senkou_param = int(tenkan_param), int(kijun_param), int(senkou_param)
+        self.position, self.quantity = position, quantity
+        self.tiker, self.figi = tiker, figi
+        self.client = client_tinkoff
+        self.until_day, self.from_day, self.interval = until_day, from_day, interval
+        # self.data = self.download_yf()
+        self.data = self.download_tinkoff()
         ichimoku = self.get_ichimoku()
         self.span_A = ichimoku.iloc(axis=1)[0]
         self.span_B = ichimoku.iloc(axis=1)[1]
@@ -31,7 +43,8 @@ class MonitoringTiker:
         self.chikou_span = ichimoku.iloc(axis=1)[4]
 
     def update_data(self):
-        self.data = self.download_yf()
+        # self.data = self.download_yf()
+        self.data = self.download_tinkoff()
         ichimoku = self.get_ichimoku()
         self.span_A = ichimoku.iloc(axis=1)[0]
         self.span_B = ichimoku.iloc(axis=1)[1]
@@ -53,7 +66,8 @@ class MonitoringTiker:
         if self.position:
             if (self.data.Low[-1] < self.kijun_sen[-1] and self.tenkan_sen[-1] < self.kijun_sen[-1]) \
                     or self.data.Low[-1] < self.span_B[-1] \
-                    or (self.data.Low[-1] < self.kijun_sen[-1] < self.tenkan_sen[-1] < self.data.Close[-2]):  # добавил - уменьшилась просадка
+                    or (self.data.Low[-1] < self.kijun_sen[-1] < self.tenkan_sen[-1] < self.data.Close[
+                -2]):  # добавил - уменьшилась просадка
                 # and (self.data.Close[-1] < self.span_A[-1] or self.data.Close[-1] < self.span_B[-1]):
                 # self.position.close()
                 self.position = False
@@ -84,24 +98,55 @@ class MonitoringTiker:
 
         if self.signal_buy():
             print(f'[+] {now} {self.tiker} Buy signal')
+            return 'Buy'
 
         elif self.signal_sell():
             print(f'[-] {now} {self.tiker} Sell signal')
-
+            return 'Sell'
         else:
             print(f"{now} {self.tiker} Нет сигналов на сделку")
-            print(f'{self.data.index.max()}')
+            print(f'{self.data.index.max()} - время последней свечи')
+            return 'No signals'
+
+    def buy_market(self, account_id):
+        r = self.client.orders.post_order(
+            order_id=str(now().timestamp()),
+            figi=self.figi,
+            quantity=self.quantity,
+            account_id=account_id,
+            direction=OrderDirection.ORDER_DIRECTION_BUY,
+            order_type=OrderType.ORDER_TYPE_MARKET
+        )
+        return r
+
+    def sell_market(self, account_id):
+        r = self.client.orders.post_order(
+            order_id=str(now().timestamp()),
+            figi=self.figi,
+            quantity=self.quantity,
+            account_id=account_id,
+            direction=OrderDirection.ORDER_DIRECTION_SELL,
+            order_type=OrderType.ORDER_TYPE_MARKET
+        )
+        return r
 
     def download_yf(self, period='1d', interval='1m'):
         yf_tiker = yf.Ticker(self.tiker)
         tiker_data = yf_tiker.history(period=period, interval=interval)
         return tiker_data
 
+    def download_tinkoff(self):
+
+        ticker = Ticker(name=self.tiker, figi=self.figi, client_tinkoff=self.client,
+                        from_day=self.from_day, until_day=self.until_day, interval=self.interval)
+        return ticker.ohlc
+
 
 def main():
     """
     1 Созд список с экземплярами класса
     2 Перебираем словарь с экземплярами
+    3 Обновляем данные
     3 запускаем трекинг
     4 обновляем позиции в словаре
     5 задержка
@@ -110,35 +155,54 @@ def main():
     #              'TAL': {'position': True, 'settings': []},
     #              'EBS': {'position': True, 'settings': []},
     #              }
+    TOKEN = os.environ["TOKEN_test_all"]
+    account_id = os.environ['account_id']
 
     with open('settings_track.json', 'r', encoding='utf8') as f:
         moex_positions = json.load(f)
 
     settings_tickers = moex_positions
-    delay = 10
+    delay = 60 * 1
+    from_day = now() - timedelta(days=31)
 
     list_obj = []
-    for tiker, param in settings_tickers.items():
-        mon_tiker = MonitoringTiker(tiker,
-                                    position=param['position'],
-                                    tenkan_param=param['settings'][0],
-                                    kijun_param=param['settings'][1],
-                                    senkou_param=param['settings'][2])
-        list_obj.append(mon_tiker)
+    with Client(TOKEN) as client:
+        for tiker, param in settings_tickers.items():
+            mon_tiker = MonitoringTiker(tiker=tiker,
+                                        figi=param['figi'],
+                                        client_tinkoff=client,
+                                        from_day=from_day,
+                                        position=param['position'],
+                                        quantity=param['quantity'],
+                                        tenkan_param=param['settings'][0],
+                                        kijun_param=param['settings'][1],
+                                        senkou_param=param['settings'][2])
+            list_obj.append(mon_tiker)
 
-    try:
-        while True:
-            for count, obj in enumerate(list_obj[0:1]):
-                obj.update_data()
-                obj.tracking()
-                obj.view()
-                #обновление позиции в словаре обьектов для вывода
-                settings_tickers[obj.tiker]['position'] = obj.position
-            print('_'*45)
-            time.sleep(delay)
-    except KeyboardInterrupt:
-        print(settings_tickers)
-        # list_obj[2].view()
+        time.sleep(delay)
+        print(f'Data uploaded \nSleep {delay}s')
+
+        try:
+            while True:
+                for count, obj in enumerate(list_obj[:]):
+                    obj.update_data()
+                    signal = obj.tracking()
+
+                    if signal == "Buy":
+                        obj.buy_market(account_id)
+                    elif signal == "Sell":
+                        obj.sell_market(account_id)
+
+                    # obj.view()
+                    # обновление позиции в словаре обьектов для вывода
+                    settings_tickers[obj.tiker]['position'] = obj.position
+                print('_' * 45)
+                time.sleep(delay)
+        except KeyboardInterrupt:
+            with open('savepoint.json', "w", encoding='utf8') as f:
+                json.dump(settings_tickers, f, indent=6)
+            print(settings_tickers)
+            # list_obj[2].view()
 
 
 if __name__ == '__main__':
